d4fa3364cfd73cfa22b4ff5fd5f57bd5e3dd6eb5
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-04-13 16:59:36 -0600

    Refactoriza modulo de modulos con estilo neomorfico

--- app/controllers/modulos_controller.rb
+++ app/controllers/modulos_controller.rb
@@ -2,7 +2,11 @@ class ModulosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @modulos = Modulo.page(params[:page]).per(5)
+    if params[:q].present?
+      @modulos = Modulo.where(Modulo.arel_table[:strNombreModulo].matches("%#{params[:q]}%")).page(params[:page]).per(5)
+    else
+      @modulos = Modulo.page(params[:page]).per(5)
+    end
     
     respond_to do |format|
       format.html
@@ -20,28 +24,50 @@ class ModulosController < ApplicationController
     render json: @modulo
   end
 
+  def new
+    @modulo = Modulo.new
+  end
+
+  def edit
+    @modulo = Modulo.find(params[:id])
+  end
+
   def create
     @modulo = Modulo.new(modulo_params)
-    if @modulo.save
-      render json: { success: true, modulo: @modulo }, status: :created
-    else
-      render json: { success: false, errors: @modulo.errors.full_messages }, status: :unprocessable_entity
+    
+    respond_to do |format|
+      if @modulo.save
+        format.html { redirect_to modulos_path, notice: 'Módulo creado exitosamente.' }
+        format.json { render json: { success: true, modulo: @modulo }, status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { success: false, errors: @modulo.errors.full_messages }, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
     @modulo = Modulo.find(params[:id])
-    if @modulo.update(modulo_params)
-      render json: { success: true, modulo: @modulo }
-    else
-      render json: { success: false, errors: @modulo.errors.full_messages }, status: :unprocessable_entity
+    
+    respond_to do |format|
+      if @modulo.update(modulo_params)
+        format.html { redirect_to modulos_path, notice: 'Módulo actualizado exitosamente.' }
+        format.json { render json: { success: true, modulo: @modulo } }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: { success: false, errors: @modulo.errors.full_messages }, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @modulo = Modulo.find(params[:id])
     @modulo.destroy
-    render json: { success: true }
+    
+    respond_to do |format|
+      format.html { redirect_to modulos_path, notice: 'Módulo eliminado exitosamente.' }
+      format.json { render json: { success: true } }
+    end
   end
 
   private
