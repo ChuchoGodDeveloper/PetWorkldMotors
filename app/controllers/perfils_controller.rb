5baeb7f88d47dd84fe402aeaabf3958850ce0aa5
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-04-13 16:18:36 -0600

    Refactoriza el modulo de perfiles con nuevo diseno

--- app/controllers/perfils_controller.rb
+++ app/controllers/perfils_controller.rb
@@ -1,52 +1,55 @@
 class PerfilsController < ApplicationController
-  skip_before_action :verify_authenticity_token # Permitir peticiones Fetch
+  before_action :require_login
+  before_action :set_perfil, only: [:show, :edit, :update, :destroy]
 
   def index
-    @perfils = Perfil.page(params[:page]).per(5)
-    
-    respond_to do |format|
-      format.html # Renderiza index.html.erb
-      format.json { 
-        render json: { 
-          data: @perfils, 
-          meta: { current_page: @perfils.current_page, total_pages: @perfils.total_pages } 
-        } 
-      }
+    if params[:q].present?
+      @perfils = Perfil.where("strNombre_Perfil ILIKE ?", "%#{params[:q]}%")
+    else
+      @perfils = Perfil.all
     end
   end
 
   def show
-    @perfil = Perfil.find(params[:id])
-    render json: @perfil
+  end
+
+  def new
+    @perfil = Perfil.new
   end
 
   def create
     @perfil = Perfil.new(perfil_params)
     if @perfil.save
-      render json: { success: true, perfil: @perfil }, status: :created
+      redirect_to perfils_path, notice: 'Perfil creado exitosamente.'
     else
-      render json: { success: false, errors: @perfil.errors.full_messages }, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
   def update
-    @perfil = Perfil.find(params[:id])
     if @perfil.update(perfil_params)
-      render json: { success: true, perfil: @perfil }
+      redirect_to perfils_path, notice: 'Perfil actualizado exitosamente.'
     else
-      render json: { success: false, errors: @perfil.errors.full_messages }, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @perfil = Perfil.find(params[:id])
     @perfil.destroy
-    render json: { success: true }
+    redirect_to perfils_path, notice: 'Perfil eliminado exitosamente.'
   end
 
   private
 
+  def set_perfil
+    @perfil = Perfil.find(params[:id])
+  end
+
   def perfil_params
+    # Asegúrate de permitir los parámetros correctos según tu schema
     params.require(:perfil).permit(:strNombre_Perfil, :bitAdministrador)
   end
 end
